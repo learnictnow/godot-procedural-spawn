15db8497b3d0e2a0313edbe2458cacaae0ed2b19
Andrew Thawley <58050814+athawley@users.noreply.github.com>
2024-07-21 09:33:01 +1200

    Added directional turning

--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var last_direction = Vector3.FORWARD
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -22,10 +23,13 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		last_direction = direction
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	$Body.rotation.y = lerp_angle($Body.rotation.y, atan2(-last_direction.x, -last_direction.z), delta * 2)
 
 	move_and_slide()
